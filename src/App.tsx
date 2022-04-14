dcfa6bd63b50033a0eb5c652ed274576b38b92cd
MaxWesley <mx_wesley@hotmail.com>
2022-04-13 23:41:11 -0300

    feat: get form values

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,48 @@
 import './index.css'
 
+import { useForm } from 'react-hook-form'
+
 function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: {
+      errors
+    }
+  } = useForm()
+  
+  const onSubmit = (data: any) => console.log(data);
+
   return (
     <div className="App">
       <section>
         <h2>Formulário com React Hook Form</h2>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <label>
             <span>Nome: </span>
             <input
               type="text"
+              {...register('nome')}
             />
           </label>
           <label>
             <span>Telefone: </span>
             <input
-              type="number"              
+              type="number"   
+              {...register('telefone')}           
             />
           </label>
           <label>
             <span>E-mail: </span>
             <input
               type="e-mail"
+              {...register('email')}
             />
           </label>
           <label>
             <span>Genero: </span>
-            <select>
+            <select {...register('genero')}>
               <option>Selecionar ...</option>
               <option>Masculino</option>
               <option>Feminino</option>
